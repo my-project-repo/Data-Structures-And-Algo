a2023b6e62acb755ab5ab12f18f4e1a8e0e4ad5f
my-project-repo <workwithimran007@gmail.com>
2025-02-26 21:18:03 +0530

    Time: 4 ms (71.69%) | Memory: 56.5 MB (56.35%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -4,14 +4,12 @@ class Solution {
         int max = 0, sum = 0, min = 0, sumTwo = 0;
         for (int i : nums) {
             sum += i;
+            sumTwo += i;
             if (sum <= 0)
                 sum = 0;
-            max = Math.max(max, sum);
-        }
-        for (int i : nums) {
-            sumTwo += i;
             if (sumTwo >= 0)
                 sumTwo = 0;
+            max = Math.max(max, sum);
             min = Math.min(sumTwo, min);
         }
         return Math.max(max, Math.abs(min));
