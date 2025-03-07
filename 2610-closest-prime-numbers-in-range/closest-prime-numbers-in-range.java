03b3f38539e4c6efa77faf95823724468204ae12
my-project-repo <workwithimran007@gmail.com>
2025-03-07 22:21:27 +0530

    Time: 1624 ms (5.20%) | Memory: 46 MB (50.40%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -1,27 +1,34 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-        int min = Integer.MAX_VALUE, first = -99;
+        int min = Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList<>();
         int[] res = { -1, -1 };
         for (int i = left; i <= right; i++) {
+            if (i!=2 && i % 2 == 0)
+                continue;
             if (isPrime(i)) {
-                if (first != -99 && i - first < min) {
-                    res[0] = first;
-                    res[1] = i;
-                    min = i - first;
-                }
-                first = i;
+                list.add(i);
+            }
+        }
+        if (list.size() < 2)
+            return res;
+        for (int i = 1; i < list.size(); i++) {
+            int diff = list.get(i) - list.get(i - 1);
+            if (diff < min) {
+                res[0] = list.get(i - 1);
+                res[1] = list.get(i);
+                min = diff;
             }
         }
         return res;
     }
 
     boolean isPrime(int n) {
-        if (n <= 1)
+        if (n == 1)
             return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
                 return false;
-            }
         }
         return true;
     }
