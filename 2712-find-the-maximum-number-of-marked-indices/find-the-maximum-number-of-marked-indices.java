60b1a05926b764150beae79e723effc3be3edb42
my-project-repo <workwithimran007@gmail.com>
2025-03-14 01:13:29 +0530

    Time: 27 ms (82.78%) | Memory: 56.6 MB (33.11%) - LeetSync

--- 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.java
+++ 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.java
@@ -2,14 +2,10 @@ class Solution {
     public int maxNumOfMarkedIndices(int[] nums) {
         Arrays.sort(nums);
         int idx = 0, edx = (nums.length + 1) / 2, count = 0;
-        while (edx < nums.length) {
-            if (2 * nums[idx] <= nums[edx]) {
-                count += 2;
+        for (; edx < nums.length; edx++)
+            if (2 * nums[idx] <= nums[edx])
                 idx++;
-            }
-            edx++;
-        }
-        return count;
+        return idx * 2;
 
     }
 }
