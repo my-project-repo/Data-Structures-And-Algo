5af72f0364dbcad1987287a69791ba421940ffde
my-project-repo <workwithimran007@gmail.com>
2025-03-26 00:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (72.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,15 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        // bruteForce
-        int res = 0,max = Integer.MIN_VALUE;
-        for (int i = 0;i<nums.length;i++)
-        {
-            if (max < nums[i])
-            {
-                max = nums[i];
-                res = i;
-            }
+        // binary Search Approach
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else
+                start = mid + 1;
         }
-        return res;
+        return end;
     }
 }
