d12f303fa74ad6c4bbf9c99f0d331a5b2ae782f6
my-project-repo <workwithimran007@gmail.com>
2025-03-27 00:41:21 +0530

    Time: 39 ms (61.31%) | Memory: 78.2 MB (11.06%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,23 +1,23 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int[] arr = new int[grid.length * grid[0].length];
-        int p = 0, mid, res = 0;
-        for (int[] a : grid) {
-            for (int i : a) {
+        int [] arr = new int[grid.length * grid[0].length];
+        int p = 0,mid, res = 0;
+        for (int []a : grid)
+        {
+            for (int i : a)
+            {
                 arr[p++] = i;
             }
         }
         Arrays.sort(arr);
-        mid = arr.length / 2;
-        for (int i : arr) {
+        mid = arr.length/2;
+        for (int i : arr)
+        {
             if (Math.abs(i - arr[mid]) % x != 0)
                 return -1;
             else
-                res += Math.abs(i - arr[mid]) / x;
+                res += Math.abs(i-arr[mid])/ x;
         }
-        return res;
+         return res;
     }
 }
