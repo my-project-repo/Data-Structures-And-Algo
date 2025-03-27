6a8054711973f28a4e7ea5c4f50b9e0c1820a9f3
my-project-repo <workwithimran007@gmail.com>
2025-03-28 00:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (35.51%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,12 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        // bruteforce
-        int[] arr = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            arr[(i + k) % nums.length] = nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = arr[i];
+        k = k % nums.length;
+        rotateFix(nums,0,nums.length-1);
+        rotateFix(nums,0,k-1);
+        rotateFix(nums,k,nums.length-1);
+    }
+    public void rotateFix (int [] nums , int start , int end)
+    {
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++; end--;
         }
     }
 }
