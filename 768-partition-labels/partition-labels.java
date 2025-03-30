db7b6aa26cc21b6040e662134f0403a2b97498f6
my-project-repo <workwithimran007@gmail.com>
2025-03-30 15:40:10 +0530

    Time: 5 ms (79.31%) | Memory: 41.9 MB (94.90%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,13 +1,17 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int max = s.lastIndexOf(s.charAt(0)),start = 0;
-        List<Integer> list = new ArrayList<>();
+         int[] lastOccurrence = new int[26];
         for (int i = 0;i<s.length();i++)
         {
-            max = Math.max(max,s.lastIndexOf(s.charAt(i)));
-            if (i==max){
-            list.add(i - start + 1);
-            start = max + 1;
+            lastOccurrence[s.charAt(i)-'a'] = i;
+        }
+        int max = s.lastIndexOf(s.charAt(0)), start = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            max = Math.max(max, lastOccurrence[s.charAt(i)-'a']);
+            if (i == max) {
+                list.add(i - start + 1);
+                start = max + 1;
             }
         }
         return list;
