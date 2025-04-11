79f8c71878d569cb52ef5b714716a0a20bfbddd9
my-project-repo <workwithimran007@gmail.com>
2025-04-11 21:21:49 +0530

    Time: 30 ms (35.35%) | Memory: 44.6 MB (65.37%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.java
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -8,19 +8,19 @@ class Solution {
         return res;
     }
 
-    boolean sumEq (int n )
-    {
-        int p = 0, sumOne = 0, sumTwo = 0 , size = (""+n).length();
-        if (size % 2 != 0) return false;
-        while (n != 0)
-        {
-            if (p < size/2)
-            sumOne += n % 10;
+    boolean sumEq(int n) {
+        String var = Integer.toString(n);
+        int size = var.length(), sumOne = 0, sumTwo = 0;
+        if (size % 2 != 0)
+            return false;
+        for (int i = 0; i < size; i++) {
+            if (i < size / 2)
+                sumOne += var.charAt(i) - '0';
             else
-            sumTwo += n%10;
-            n/=10;
-            p++;
+                sumTwo += var.charAt(i) - '0';
+
         }
         return sumOne == sumTwo;
+
     }
 }
