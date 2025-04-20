9d95c62049cdea1b2cb3dfb7d87c1bbe0a6248c0
my-project-repo <workwithimran007@gmail.com>
2025-04-20 21:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (90.69%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,14 +1,12 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        int[] freq = new int[1000];
         int ans = 0;
         for (int i : answers) {
-            freq.put(i, freq.getOrDefault(i, 0) + 1);
-        }
-        for (int i : freq.keySet()) {
-            int size = i + 1;
-            int groups = (int) Math.ceil(freq.get(i) * 1.0 / size);
-            ans += groups * size;
+            if (++freq[i] == 1)
+                ans += i + 1;
+            if (freq[i] == i + 1)
+                freq[i] = 0;
         }
         return ans;
     }
