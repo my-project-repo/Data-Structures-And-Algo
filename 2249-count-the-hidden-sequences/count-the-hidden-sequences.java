a4422c23fd012c1a90fc4acb042ea6c4cbb8b782
my-project-repo <workwithimran007@gmail.com>
2025-04-22 00:27:49 +0530

    Time: 3 ms (100.00%) | Memory: 61.9 MB (31.75%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.java
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.java
@@ -3,8 +3,8 @@ class Solution {
         long max = 0, min = 0, sum = 0;
         for (int i : differences) {
             sum += i;
-            max = Math.max(max, sum);
-            min = Math.min(min, sum);
+            max = (max < sum) ? sum : max;
+            min = (min > sum) ? sum : min;
         }
         return (int) Math.max(0, upper - lower - max + min + 1);
     }
