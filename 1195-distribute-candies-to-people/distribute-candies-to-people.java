3d3b81f38f2ad44c723b66ae8b0ede2e947cb500
my-project-repo <workwithimran007@gmail.com>
2025-04-25 23:22:42 +0530

    Time: 1 ms (97.10%) | Memory: 41.1 MB (75.57%) - LeetSync

--- 1195-distribute-candies-to-people/distribute-candies-to-people.java
+++ 1195-distribute-candies-to-people/distribute-candies-to-people.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] distributeCandies(int candies, int num_people) {
         int[] res = new int[num_people];
-        int k = 0, counter = 1;
+        int k = 0, counter = 1; // k serves as the index of the array
         while (candies > 0) {
             int index = k % num_people;
-            if (counter > candies) {
-                res[index] += candies;
+            if (counter > candies) { // when there are not sufficient candies
+                res[index] += candies; // we transfer the remaining candies
                 break;
             }
-            res[index] += counter;
-            candies -= counter;
-            counter++;
-            k++;
+            res[index] += counter; // adding candies to people ; array
+            candies -= counter; // current number of candies after giving candies
+            counter++; // adding the number of candies to be given by 1
+            k++; 
         }
-        return res;
+        return res; // finally returning the result array
     }
 }
