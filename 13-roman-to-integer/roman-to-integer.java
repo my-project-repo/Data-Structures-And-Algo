b5e50a4d0cd232350895b05d825c0a176b03a5e8
my-project-repo <workwithimran007@gmail.com>
2025-04-26 14:24:54 +0530

    Time: 4 ms (67.36%) | Memory: 44.8 MB (69.52%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -5,13 +5,7 @@ class Solution {
     public int romanToInt(String s) {
         int res = 0;
         Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        adder(map);
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (i < s.length() - 1) {
@@ -35,4 +29,13 @@ class Solution {
         return res;
     }
 
+    private void adder(Map<Character, Integer> map) {
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+    }
 }
