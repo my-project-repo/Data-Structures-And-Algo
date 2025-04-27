f8d1ca41ff8ab75fe8caae0ff3df9eda93515699
my-project-repo <workwithimran007@gmail.com>
2025-04-27 16:06:08 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (29.98%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
@@ -1,5 +1,6 @@
 class Solution {
     public int countSubarrays(int[] nums) {
+        // sliding window of fixed size ( 3 )
         int mid = 1, window = 0, res = 0, k = 0;
         for (int i = 0; i < 3; i++) {
             window += nums[i];
