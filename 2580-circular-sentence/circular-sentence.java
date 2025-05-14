26b56a919762ec98f25942d2e784696a26993aea
my-project-repo <workwithimran007@gmail.com>
2025-05-14 23:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (22.16%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        char[] arr = sentence.toCharArray();
-        int n = arr.length;
-        char last = ' ';
+        int n = sentence.length();
         if (sentence.charAt(0) != sentence.charAt(n - 1))
             return false;
-        for (int i = 0; i < n; i++) {
-            if (i < n - 1 && Character.isLetter(arr[i]) && arr[i + 1] == ' ')
-                last = arr[i];
-            if (i < n - 1 && arr[i] == ' ' && Character.isLetter(arr[i + 1])) {
-                if (last != arr[i + 1])
-                    return false;
-            }
+        int point = sentence.indexOf(" ");
+        if (point == -1)
+            return true;
+        while (point != -1) {
+            if (sentence.charAt(point - 1) != sentence.charAt(point + 1))
+                return false;
+            point = sentence.indexOf(" ", point + 1);
         }
         return true;
     }
