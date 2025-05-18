a0a1a7d95262eb1a829c3f82a5961ed893c0e068
my-project-repo <workwithimran007@gmail.com>
2025-05-18 23:42:13 +0530

    Time: 47 ms (26.79%) | Memory: 113.7 MB (43.75%) - LeetSync

---
+++ 2778-frequency-tracker/frequency-tracker.java
@@ -0,0 +1,53 @@
+class FrequencyTracker {
+    Map<Integer, Integer> map;
+    Map<Integer, Integer> mapFreq;
+
+    public FrequencyTracker() {
+        map = new HashMap<>();
+        mapFreq = new HashMap<>();
+    }
+
+    public void add(int number) {
+        int oldCount = map.getOrDefault(number, 0);
+        int newCount = oldCount + 1;
+        map.put(number, newCount);
+        mapFreq.put(oldCount, mapFreq.getOrDefault(oldCount, 1) - 1);
+        mapFreq.put(newCount, mapFreq.getOrDefault(newCount, 0) + 1);
+    }
+
+    public void deleteOne(int number) {
+        if (!map.containsKey(number))
+            return;
+        int oldcount = map.get(number);
+        int newcount = oldcount - 1;
+        mapFreq.put(oldcount, mapFreq.getOrDefault(oldcount, 1) - 1);
+        if (mapFreq.get(oldcount) == 0) {
+            mapFreq.remove(oldcount);
+        }
+        if (newcount == 0) {
+            map.remove(number);
+        } else {
+            map.put(number, newcount);
+            mapFreq.put(newcount, mapFreq.getOrDefault(newcount, 0) + 1);
+        }
+    }
+
+    public boolean hasFrequency(int frequency) {
+        return mapFreq.getOrDefault(frequency, 0) > 0;
+    }
+}
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker obj = new FrequencyTracker();
+ * obj.add(number);
+ * obj.deleteOne(number);
+ * boolean param_3 = obj.hasFrequency(frequency);
+ */
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker obj = new FrequencyTracker();
+ * obj.add(number);
+ * obj.deleteOne(number);
+ * boolean param_3 = obj.hasFrequency(frequency);
+ */
