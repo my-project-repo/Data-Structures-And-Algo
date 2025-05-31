e7c7283a200b696560ab5e72d6a981e1f929fdc6
my-project-repo <workwithimran007@gmail.com>
2025-05-31 23:12:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (48.84%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,11 +1,13 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if (n % 2 == 0) return gcm (2 , n , n);
+        if (n % 2 == 0)
+            return lcm(2, n, n);
         return 2 * n;
     }
-    int gcm (int x , int y , int temp)
-    {
-        if (y == 0) return 2 * temp / x;
-        return gcm (y , x % y, temp);
+
+    int lcm(int x, int y, int temp) {
+        if (y == 0)
+            return 2 * temp / x;
+        return lcm(y, x % y, temp);
     }
 }
