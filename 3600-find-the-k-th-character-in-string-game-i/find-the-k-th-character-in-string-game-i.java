c8aca3a9083545c151526f80680c4e9d63af14e9
my-project-repo <workwithimran007@gmail.com>
2025-07-03 14:56:37 +0530

    Time: 2 ms (79.80%) | Memory: 42 MB (71.03%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -5,7 +5,7 @@ class Solution {
             int n = str.length();
             for (int i = 0; i < n; i++) {
                 int ch = str.charAt(i);
-                str.append((ch == 122) ? 'a' : (char) (ch + 1));
+                str.append((char) (ch + 1));
             }
         }
         return str.charAt(k - 1);
