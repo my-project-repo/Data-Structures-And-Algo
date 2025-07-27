9c23e615331a66b952043f303885b379f3d6e86d
my-project-repo <workwithimran007@gmail.com>
2025-07-28 01:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (93.24%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,26 +1,16 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int size = 0, res = 0, p = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i < nums.length - 1 && nums[i] == nums[i + 1])
-                continue;
-            size++;
+        int count = 0;
+        int left = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+                        (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    count++;
+                }
+                left = i;
+            }
         }
-        int[] deductedArr = new int[size];
-        for (int i = 0; i < nums.length; i++) {
-            if (i < nums.length - 1 && nums[i] == nums[i + 1])
-                continue;
-            deductedArr[p++] = nums[i];
-        }
-        for (int i = 2; i < size; i++) {
-            int first = deductedArr[i - 2];
-            int second = deductedArr[i - 1];
-            int third = deductedArr[i];
-            if (second > first && second > third)
-                res++;
-            else if (second < first && second < third)
-                res++;
-        }
-        return res;
+        return count;
     }
 }
