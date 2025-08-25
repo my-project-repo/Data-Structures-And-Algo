a376cecb36683a8f235a6df268a740d5066941e3
my-project-repo <workwithimran007@gmail.com>
2025-08-25 22:32:25 +0530

    Time: 200 ms (69.33%) | Memory: 59.6 MB (21.33%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.java
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.java
@@ -2,17 +2,17 @@ class Solution {
     public int halveArray(int[] nums) {
         Queue<Double> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         int res = 0;
-        double find = 0.0, sum = 0.0;
+        double sum = 0.0;
         for (int i : nums) {
             maxHeap.add(i * 1.0);
             sum += i;
         }
-        find = sum / 2;
+        double find = sum / 2;
         while (sum > find) {
+            double val = maxHeap.poll() / 2;
+            sum -= val;
+            maxHeap.add(val);
             res++;
-            double val = maxHeap.poll();
-            sum -= val / 2;
-            maxHeap.add(val / 2);
         }
         return res;
     }
