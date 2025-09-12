b2ea561d4b1c19f03fb07d67271e591d132e22d3
my-project-repo <workwithimran007@gmail.com>
2025-09-13 01:42:28 +0530

    Time: 7 ms (55.10%) | Memory: 45.6 MB (66.33%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int vowel = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowel++;
+            }
+        }
+        return vowel != 0;
+    }
+}
