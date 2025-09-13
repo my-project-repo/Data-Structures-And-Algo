1a45764c0a5dd20e9168221483b8baaeccdb3a29
my-project-repo <workwithimran007@gmail.com>
2025-09-13 23:00:06 +0530

    Time: 35 ms (50.91%) | Memory: 62.2 MB (28.36%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -1,16 +1,11 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
         long minus = 0, res = 0;
-        Queue<Integer> max = new PriorityQueue<>((a,b)->b-a);
-        for (int i : happiness)
-        {
-            max.add(i);
-        }
-        while (k > 0 && !max.isEmpty())
-        {
-            res += Math.max(max.poll() - minus , 0);
+        int n = happiness.length;
+        Arrays.sort(happiness);
+        for (int i = n - 1; i >= n - k; i--) {
+            res += Math.max(happiness[i] - minus, 0);
             minus++;
-            k--;
         }
         return res;
     }
