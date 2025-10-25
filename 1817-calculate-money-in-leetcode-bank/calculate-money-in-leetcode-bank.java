39f646f0f80d357ad54b6c5898a33fdf6697913a
my-project-repo <workwithimran007@gmail.com>
2025-10-26 00:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (76.33%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int totalMoney(int n) {
+        int div = n / 7, rem = n - (div * 7), res = 0, k = 0, t = div + 1;
+        while (div-- > 0) {
+            res += 28 + k;
+            k += 7;
+        }
+        for (int i = 0; i < rem; i++) {
+            res += t++;
+        }
+        return res;
+    }
+}
