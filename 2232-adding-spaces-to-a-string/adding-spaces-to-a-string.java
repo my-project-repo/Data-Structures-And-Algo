8266ea7e52c7a83be869b5e4af2206585126cfb6
my-project-repo <workwithimran007@gmail.com>
2025-11-07 00:14:01 +0530

    Time: 23 ms (66.00%) | Memory: 89.8 MB (7.53%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
@@ -1,15 +1,13 @@
 class Solution {
     public String addSpaces(String s, int[] spaces) {
         StringBuilder str = new StringBuilder();
-        int i = 0 , p = 0, n = spaces.length;
-        while (i < s.length())
-        {
-            if (p < n && spaces[p] == i)
-            {
+        int p = 0, n = spaces.length;
+        for (int i = 0; i < s.length(); i++) {
+            if (p < n && i == spaces[p]) {
                 str.append(" ");
                 p++;
             }
-            str.append(s.charAt(i++));
+                str.append(s.charAt(i));
         }
         return str.toString();
     }
