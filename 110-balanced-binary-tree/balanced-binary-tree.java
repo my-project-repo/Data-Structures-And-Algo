cb608da9d7dbe073055489e64ae8062c417ca463
my-project-repo <workwithimran007@gmail.com>
2025-12-01 17:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (8.02%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -23,10 +23,8 @@ class Solution {
             return 0;
         int left = height(root.left);
         int right = height(root.right);
-        if (left == -1 || right == -1)
-            return -1;
-        if (Math.abs(left - right) > 1)
-            return -1;
+        if (left == -1 || right == -1) return -1;
+        if (Math.abs(left - right) > 1)return -1;
         return 1 + Math.max(left, right);
     }
 }
