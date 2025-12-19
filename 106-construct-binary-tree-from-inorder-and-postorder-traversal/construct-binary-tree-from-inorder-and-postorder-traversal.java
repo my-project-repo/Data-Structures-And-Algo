7e1a866d60dd834542ff7f666f1716843787ea0d
my-project-repo <workwithimran007@gmail.com>
2025-12-20 00:32:37 +0530

    Time: 1 ms (98.16%) | Memory: 46.5 MB (12.41%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,22 +14,25 @@
  * }
  */
 class Solution {
-    Map<Integer,Integer> map = new HashMap<>();
+    Map<Integer, Integer> map = new HashMap<>();
     int p;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int n = inorder.length;
-        p = postorder.length-1;
-        for (int i = 0;i<inorder.length;i++) map.put(inorder[i],i);
-        return helper(0,n-1,postorder);
+        p = postorder.length - 1;
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        return helper(0, n - 1, postorder);
     }
-    TreeNode helper (int start , int end , int [] post)
-    {
-        if (start > end) return null;
+
+    TreeNode helper(int start, int end, int[] post) {
+        if (start > end)
+            return null;
         int curr = post[p--];
-        TreeNode node = new TreeNode (curr);
+        TreeNode node = new TreeNode(curr);
         int pos = map.get(curr);
-        node.right = helper(pos+1,end,post);
-        node.left = helper(start,pos-1,post);
+        node.right = helper(pos + 1, end, post);
+        node.left = helper(start, pos - 1, post);
         return node;
 
     }
