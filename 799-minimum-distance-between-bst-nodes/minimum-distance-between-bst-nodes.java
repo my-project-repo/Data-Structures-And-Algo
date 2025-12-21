58dc41b7017acbf7b72a96dbcc5ded14a0a69e06
my-project-repo <workwithimran007@gmail.com>
2025-12-22 01:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (53.16%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
@@ -15,18 +15,19 @@
  */
 class Solution {
     List<Integer> list = new ArrayList<>();
+
     public int minDiffInBST(TreeNode root) {
         int min = Integer.MAX_VALUE;
         dfs(root);
-        for (int i = 1;i<list.size();i++)
-        {
-            min = Math.min(min,list.get(i) - list.get(i-1));
+        for (int i = 1; i < list.size(); i++) {
+            min = Math.min(min, list.get(i) - list.get(i - 1));
         }
         return min;
     }
-    void dfs (TreeNode root)
-    {
-        if (root == null) return;
+
+    void dfs(TreeNode root) {
+        if (root == null)
+            return;
         dfs(root.left);
         list.add(root.val);
         dfs(root.right);
