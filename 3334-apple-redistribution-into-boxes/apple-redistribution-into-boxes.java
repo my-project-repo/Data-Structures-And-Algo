eca4776efe582d7e4ef9dea334b801a1f645eadb
my-project-repo <workwithimran007@gmail.com>
2025-12-25 02:05:30 +0530

    Time: 4 ms (83.53%) | Memory: 44.6 MB (21.18%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -3,7 +3,7 @@ class Solution {
         int sum = 0, res = 0;
         for (int i : apple)
             sum += i;
-            Arrays.sort(capacity);
+        Arrays.sort(capacity);
         for (int i = capacity.length - 1; i >= 0; i--) {
             if (sum > 0) {
                 res++;
