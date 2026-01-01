52b31be58bce00273f1e788f250bbe77fd4cc918
my-project-repo <workwithimran007@gmail.com>
2026-01-02 00:59:59 +0530

    Time: 1 ms (98.14%) | Memory: 46.9 MB (39.85%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,19 +1,16 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        List<Integer> list = new LinkedList<>();
-        int n = arr.length, c = 0;
-        for (int i : arr) {
-            list.add(i);
-            c++;
-            if (i == 0) {
-                list.add(0);
-                c++;
+        int n = arr.length, p = 0;
+        int [] temp = new int[n];
+        for (int i : arr)
+        {
+            temp[p++] = i;
+            if (i == 0 && p < n)
+            {
+                temp[p++] = 0;
             }
-            if (c == n)
-                break;
+            if (p == n)  break;
         }
-        for (int i = 0; i < n; i++) {
-            arr[i] = list.get(i);
-        }
+        for (int i = 0;i<n;i++) arr[i] = temp[i];
     }
 }
