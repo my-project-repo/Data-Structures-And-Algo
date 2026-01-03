4ae78c29000af1cb83a21ae2cf3445874e636131
my-project-repo <workwithimran007@gmail.com>
2026-01-03 19:52:05 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (47.45%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,40 +14,26 @@
  * }
  */
 class Solution {
-    List<TreeNode> list;
+    TreeNode prev = null, first = null, second = null;
+
     public void recoverTree(TreeNode root) {
-        list = new LinkedList<>();
         dfs(root);
-        TreeNode idx = null, edx = null;
-        int n = list.size();
-        for (int i = 1;i<n;i++)
-        {
-            if (list.get(i).val < list.get(i-1).val){
-            idx = list.get(i-1);
-            break;
-            }
-        }
-        for (int i = n-1;i>=1;i--)
-        {
-            if (list.get(i).val < list.get(i-1).val)
-            {
-                edx = list.get(i);
-                break;
-            }
-        }
-        if (idx != null && edx != null)
-        {
-            int temp = idx.val;
-            idx.val = edx.val;
-            edx.val = temp;
-        }
-       
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
-    void dfs (TreeNode root)
-    {
-        if (root == null) return;
+
+    void dfs(TreeNode root) {
+        if (root == null)
+            return;
         dfs(root.left);
-        list.add(root);
+        if (prev != null && prev.val > root.val) {
+            if (first == null) {
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
         dfs(root.right);
     }
 }
