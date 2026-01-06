036d6bf08ab57a43ec0a1c625b5eaa728cd6f977
my-project-repo <workwithimran007@gmail.com>
2026-01-07 00:10:25 +0530

    Time: 2 ms (98.90%) | Memory: 84.4 MB (18.16%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -1,10 +1,10 @@
 class Solution {
-    public long getDescentPeriods(int[] prices) {
+    public long getDescentPeriods(int[] p) {
         long res = 1, sum = 1;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i - 1] - prices[i] == 1) {
+        for (int i = 1; i < p.length; i++) {
+            if ((p[i-1] - p[i]) == 1)
                 sum++;
-            } else
+            else
                 sum = 1;
             res += sum;
         }
