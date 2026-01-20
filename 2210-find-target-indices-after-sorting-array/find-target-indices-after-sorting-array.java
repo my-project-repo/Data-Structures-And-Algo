82b82cbde9e742b6fb4fab06508b9a44139adb9a
my-project-repo <workwithimran007@gmail.com>
2026-01-20 22:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (69.92%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         List<Integer> list = new ArrayList<>();
-        int count = 0;
-        int [] freq = new int[101];
-        for (int i : nums) freq[i]++;
-        for (int i = 1;i<101;i++)
+        int same = 0 , diff = 0;
+        for (int i : nums)
         {
-            if (freq[i] == 0) continue;
-            if (i == target)
-            {
-                while (freq[i]-->0)
-                {
-                    list.add(count);
-                    count++;
-                }
-            } else
-                count += freq[i];
+            if (i < target)
+            diff++;
+            if (i == target) same++;
         }
+        while (same --> 0)
+        {
+            list.add(diff);
+            diff++;
+        }
+
         return list;
     }
 }
