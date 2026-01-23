2572f58d7f89d2fcd760772257fac5669a6a7081
my-project-repo <workwithimran007@gmail.com>
2026-01-23 19:07:29 +0530

    Time: 2 ms (99.86%) | Memory: 92.8 MB (84.67%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int [] ans = new int[arr.length];
-        int [] res = new int[queries.length];
+        int[] ans = new int[arr.length];
+        int[] res = new int[queries.length];
         int p = 0;
         ans[0] = arr[0];
-        for (int i = 1;i<arr.length;i++)
-        ans[i] = arr[i] ^ ans[i-1];
-        for (int [] q : queries)
-        {
-            int i = q[0] , j = q[1];
+        for (int i = 1; i < arr.length; i++)
+            ans[i] = arr[i] ^ ans[i - 1];
+        for (int[] q : queries) {
+            int i = q[0], j = q[1];
             if (i == 0)
-            res[p] = ans[j];
+                res[p] = ans[j];
             else
-            res[p] = ans[j] ^ ans[i-1];
+                res[p] = ans[j] ^ ans[i - 1];
             p++;
         }
         return res;
