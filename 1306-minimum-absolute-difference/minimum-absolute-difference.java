b2a38c44f82b2657e1635162bf746e7f40082d2c
my-project-repo <workwithimran007@gmail.com>
2026-01-26 22:17:23 +0530

    Time: 20 ms (97.68%) | Memory: 64 MB (46.18%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -5,11 +5,11 @@ class Solution {
         Arrays.sort(nums);
         for (int i = 1;i<nums.length;i++)
         {
-            min = Math.min(min,Math.abs(nums[i] - nums[i-1]));
+            min = Math.min(min,nums[i] - nums[i-1]);
         }
         for (int i = 1;i<nums.length;i++)
         {
-            if (Math.abs(nums[i] - nums[i-1]) == min)
+            if (nums[i] - nums[i-1] == min)
             {
                 list.add(Arrays.asList(nums[i-1] , nums[i]));
             }
