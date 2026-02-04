043ac5e4b8a41050c14fdda5d42a170e940d7e00
my-project-repo <workwithimran007@gmail.com>
2026-02-05 01:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (52.36%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -7,16 +7,19 @@ class Solution {
             i++;
             a = true;
         }
+        if (!a) return a;
         while (i < n && nums[i] < nums[i-1])
         {
             i++;
             b = true;
         }
+        if (!b) return b;
         while (i < n && nums[i] > nums[i-1])
         {
             i++;
             c = true;
         }
+        if (!c) return c;
         return i == nums.length && a && b && c;
     }
 }
