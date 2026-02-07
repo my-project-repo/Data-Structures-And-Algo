055e94dab93cd05a62355a8ac755388e992e7611
my-project-repo <workwithimran007@gmail.com>
2026-02-07 22:39:18 +0530

    Time: 150 ms (5.03%) | Memory: 46.8 MB (47.13%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,27 +1,28 @@
 class Solution {
     int max = 0;
+
     public int maxLength(List<String> arr) {
-        solver(0,arr.size(),arr,"");
+        solver(0, arr.size(), arr, "");
         return max;
     }
-    void solver (int i , int n , List<String> str, String temp)
-    {
-        if (i == n)
-        {
-            if (temp.length() > max && !hasDuplicate(temp))
-            {
+
+    void solver(int i, int n, List<String> str, String temp) {
+        if (i == n) {
+            if (temp.length() > max && !hasDuplicate(temp)) {
                 max = temp.length();
             }
             return;
         }
-        solver(i+1,n,str,temp+str.get(i)); // include
-        solver(i+1,n,str,temp); // exclude
+        solver(i + 1, n, str, temp + str.get(i)); // include
+        solver(i + 1, n, str, temp); // exclude
     }
-    boolean hasDuplicate(String curr){
+
+    boolean hasDuplicate(String curr) {
         int[] val = new int[26];
 
-        for(char c : curr.toCharArray()){
-            if(val[c - 'a'] > 0) return true;
+        for (char c : curr.toCharArray()) {
+            if (val[c - 'a'] > 0)
+                return true;
             val[c - 'a']++;
         }
         return false;
