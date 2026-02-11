59c5ab88375c40a017d080ad899e63bf6a60e1d8
my-project-repo <workwithimran007@gmail.com>
2026-02-12 01:50:42 +0530

    Time: 5 ms (98.09%) | Memory: 130.2 MB (94.06%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -1,37 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        head = reverse(head);
-        ListNode ans = new ListNode(-1);
-        ListNode temp = ans;
-        int max = -99;
-        while (head != null) {
-
-            if (max <= head.val) {
-                temp.next = new ListNode(head.val);
-                temp = temp.next;
+        int max = Integer.MIN_VALUE;
+        ListNode h = reverse(head);
+        ListNode temp = null;
+        while (h != null)
+        {
+            ListNode next = h.next;
+            if (max <= h.val)
+            {
+                h.next = temp;
+                temp = h;
+                max = h.val;
             }
-            max = Math.max(max,head.val);
-            head = head.next;
+            h = next;
         }
-        return reverse(ans.next);
-
+        return temp;
     }
-
-    ListNode reverse(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode next;
-        while (curr != null) {
+    ListNode reverse (ListNode head)
+    {
+        ListNode next , prev = null , curr = head;
+        while (curr != null)
+        {
             next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -39,5 +38,4 @@ class Solution {
         }
         return prev;
     }
-
 }
