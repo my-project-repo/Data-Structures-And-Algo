d4f904628e5cfbbd959a91a311fe7b7b7c732009
my-project-repo <workwithimran007@gmail.com>
2026-02-12 14:03:47 +0530

    Time: 92 ms (88.39%) | Memory: 47.3 MB (7.68%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -2,6 +2,7 @@ class Solution {
     public int longestBalanced(String s) {
         int max = 0;
         for (int i = 0; i < s.length(); i++) {
+            if (max > s.length()-i) break;
             int[] freq = new int[26];
             for (int j = i; j < s.length(); j++) {
                 char ch = s.charAt(j);
