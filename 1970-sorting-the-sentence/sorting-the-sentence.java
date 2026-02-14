e316066dc0c5fd5efdba56ff5084ecf3c8ef435e
my-project-repo <workwithimran007@gmail.com>
2026-02-14 21:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (67.05%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -1,20 +1,9 @@
 class Solution {
     public String sortSentence(String s) {
-      String [] arr = new String [9];
-      String [] words = s.split(" ");
-      StringBuilder str = new StringBuilder();
-      for (String word : words)
-      {
-        int n = word.length();
-        int idx = word.charAt(n-1) - '0';
-        arr[idx-1] = word.substring(0,n-1);
-      }
-      for (String word : arr)
-      {
-        if (word == null) break;
-        str.append(word+" ");
-      }
-      return str.toString().trim();
-       
+        String[] words = s.split(" ");
+        String[] res =new String[words.length];
+        for (String w : words) 
+            res[w.charAt(w.length() - 1) - '1'] = w.substring(0, w.length() - 1);
+        return String.join(" ", res);
     }
 }
