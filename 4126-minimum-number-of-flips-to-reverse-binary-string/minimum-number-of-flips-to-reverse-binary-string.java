1dbe1f5034e4f72cf4a02e125f88a369d450cf08
my-project-repo <workwithimran007@gmail.com>
2026-02-24 00:07:42 +0530

    Time: 1 ms (99.42%) | Memory: 42.8 MB (72.09%) - LeetSync

--- 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.java
+++ 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.java
@@ -1,14 +1,12 @@
 class Solution {
     public int minimumFlips(int n) {
         int r = 0, temp = n, l = 0, res = 0;
-        while (temp > 0)
-        {
+        while (temp > 0) {
             r++;
             temp >>= 1;
         }
         r--;
-        while (l < r)
-        {
+        while (l < r) {
             res += ((n >> r) & 1) ^ ((n >> l) & 1);
             l++;
             r--;
