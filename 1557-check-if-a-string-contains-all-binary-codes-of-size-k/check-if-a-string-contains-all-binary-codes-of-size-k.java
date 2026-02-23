98db91f0fe14f9319c7fa906732e1f275f64433d
my-project-repo <workwithimran007@gmail.com>
2026-02-23 23:44:18 +0530

    Time: 108 ms (81.98%) | Memory: 83.5 MB (72.84%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean hasAllCodes(String s, int k) {
         Set<String> set = new HashSet<>();
         int count = 1 << k;
+        if (s.length() < count ) return false;
         for (int i = 0; i <= s.length() - k; i++) {
             String val = s.substring(i, i + k);
             if (!set.contains(val)) {
