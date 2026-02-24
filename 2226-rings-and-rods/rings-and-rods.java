979a01b298dd6a8ac5e955975ab827047aa42f7a
my-project-repo <workwithimran007@gmail.com>
2026-02-25 01:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (77.60%) - LeetSync

---
+++ 2226-rings-and-rods/rings-and-rods.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPoints(String rings) {
+        int res = 0;
+        int[] b = new int[10];
+        int[] r = new int[10];
+        int[] g = new int[10];
+        char[] ch = rings.toCharArray();
+        for (int i = 1; i < ch.length; i += 2) {
+            if (Character.isDigit(ch[i])) {
+                int idx = ch[i] - '0';
+                if (ch[i - 1] == 'B')
+                    b[idx]++;
+                else if (ch[i - 1] == 'R')
+                    r[idx]++;
+                else
+                    g[idx]++;
+            }
+        }
+        for (int i = 0;i<10;i++)
+        {
+            if (b[i] >= 1 && r[i] >= 1 && g[i] >= 1)
+            res++;
+        }
+        return res;
+    }
+}
