e19b327c7a5e27e0bbf43d21988efa9e385ac317
my-project-repo <workwithimran007@gmail.com>
2026-03-10 15:02:32 +0530

    Time: 12 ms (51.01%) | Memory: 56.6 MB (50.25%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,24 +1,19 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int[] d : dp)
-            Arrays.fill(d, -1);
-        return rec(0, coins, amount, dp);
-    }
+        for (int i = 0; i <= coins.length; i++) {
+            dp[i][0] = 1;
+        }
 
-    int rec(int idx, int[] arr, int target, int[][] dp) {
-        if (target == 0)
-            return 1;
-        if (idx >= arr.length)
-            return 0;
-        if (dp[idx][target] != -1)
-            return dp[idx][target];
-        if (arr[idx] > target)
-            return rec(idx + 1, arr, target, dp);
-        int includeAgain = rec(idx, arr, target - arr[idx], dp);
-        int exclude = rec(idx + 1, arr, target, dp);
-        return dp[idx][target] = includeAgain + exclude;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                } else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[dp.length - 1][amount];
 
     }
-
 }
