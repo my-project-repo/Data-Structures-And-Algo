96e364acf9a35e4afb6e5509183c17ae28976acc
my-project-repo <workwithimran007@gmail.com>
2026-03-13 00:14:56 +0530

    Time: 3 ms (36.19%) | Memory: 46 MB (31.36%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        solver(0, candidates, target, new ArrayList<>(), res);
-        return res;
 
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        sum(0, candidates, target, new ArrayList<>(), list);
+        return list;
     }
 
-    void solver(int idx, int[] arr, int target, List<Integer> list, List<List<Integer>> res) {
+    void sum(int idx, int[] nums, int target, List<Integer> list, List<List<Integer>> res) {
         if (target == 0) {
             res.add(new ArrayList<>(list));
             return;
         }
-        if (idx >= arr.length)
+        if (idx >= nums.length || nums[idx] > target) {
             return;
-        if (arr[idx] > target)
-            solver(idx + 1, arr, target, list, res);
-        else {
-
-            list.add(arr[idx]);
-            solver(idx, arr, target - arr[idx], list, res);
-            list.remove(list.size() - 1);
-            solver(idx + 1, arr, target, list, res);
         }
+        if (idx + 1 < nums.length && nums[idx] == nums[idx+1])
+        sum(idx+1,nums,target,list,res);
+
+        list.add(nums[idx]); // include;
+        sum(idx, nums, target - nums[idx], list, res);
+        list.removeLast(); //exclude
+        sum(idx + 1, nums, target, list, res);
+
     }
 }
