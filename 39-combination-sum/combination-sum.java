49aac74fdd0ee365061564caff06d329fa952f1b
my-project-repo <workwithimran007@gmail.com>
2026-03-13 00:18:49 +0530

    Time: 2 ms (95.47%) | Memory: 45.9 MB (48.84%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(candidates);
         sum(0, candidates, target, new ArrayList<>(), list);
         return list;
     }
@@ -12,11 +11,7 @@ class Solution {
             res.add(new ArrayList<>(list));
             return;
         }
-        if (idx >= nums.length || nums[idx] > target) {
-            return;
-        }
-        if (idx + 1 < nums.length && nums[idx] == nums[idx+1])
-        sum(idx+1,nums,target,list,res);
+        if (idx >= nums.length || target < 0) return;
 
         list.add(nums[idx]); // include;
         sum(idx, nums, target - nums[idx], list, res);
