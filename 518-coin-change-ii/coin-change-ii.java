2416b640a13255fbf8c013c6d5cbb2fff2402335
my-project-repo <workwithimran007@gmail.com>
2026-03-13 00:31:34 +0530

    Time: 165 ms (5.17%) | Memory: 56.5 MB (53.97%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int i = 0; i <= coins.length; i++) {
-            dp[i][0] = 1;
-        }
+        int [][] dp = new int[coins.length+1][amount+1];
+        for (int [] d : dp) Arrays.fill(d,-1);
+        return coin(0, coins, amount,dp);
+    }
 
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                if (coins[i - 1] <= j) {
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-                } else
-                    dp[i][j] = dp[i - 1][j];
-            }
-        }
-        return dp[dp.length - 1][amount];
-
+    int coin(int idx, int[] nums, int target, int[][] dp) {
+        if (target == 0)
+            return 1;
+        if (idx >= nums.length)
+            return 0;
+        if (dp[idx][target] != -1)
+            return dp[idx][target];
+        if (nums[idx] > target)
+            return coin(idx + 1, nums, target,dp);
+        int include = coin(idx, nums, target - nums[idx],dp);
+        int exclude = coin(idx + 1, nums, target,dp);
+        return dp[idx][target] = include + exclude;
     }
 }
