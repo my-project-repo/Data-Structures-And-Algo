cbb7eaa5875b5a7f2c54ba53f263d55e6e4b36c6
my-project-repo <workwithimran007@gmail.com>
2026-04-02 23:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (20.75%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.java
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isCovered(int[][] ranges, int left, int right) {
-        int [] line = new int[52];
-        for (int [] r : ranges)
-        {
+        int[] line = new int[52];
+        for (int[] r : ranges) {
             line[r[0]]++;
-            line[r[1]+1]--;
+            line[r[1] + 1]--;
         }
-        for (int i = 1;i<52;i++)
-        {
-            line[i] += line[i-1];
+        for (int i = 1; i < 52; i++) {
+            line[i] += line[i - 1];
         }
-        for (int i = left;i<=right;i++)
-        {
-            if (line[i] == 0) return false;
+        for (int i = left; i <= right; i++) {
+            if (line[i] == 0)
+                return false;
         }
         return true;
     }
