87559d7feb2899c0cbef0b1787d173d448a9c6ee
my-project-repo <workwithimran007@gmail.com>
2026-04-04 00:20:52 +0530

    Time: 2 ms (98.58%) | Memory: 48.3 MB (6.57%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,25 +14,29 @@
  * }
  */
 class Solution {
-    List<Integer> l = new ArrayList<>();
+   int [] arr = new int[10001];
+   int p = 0;
+
     public boolean findTarget(TreeNode root, int k) {
         dfs(root);
-        int idx = 0 , edx = l.size()-1;
-        while (idx < edx)
-        {
-            int sum = l.get(idx) + l.get(edx);
-            if (sum == k) return true;
+        int idx = 0, edx = p - 1;
+        while (idx < edx) {
+            int sum = arr[idx] + arr[edx];
+            if (sum == k)
+                return true;
             else if (sum > k)
-            edx--;
-            else idx++;
+                edx--;
+            else
+                idx++;
         }
         return false;
     }
-    void dfs (TreeNode root)
-    {
-        if (root == null) return;
+
+    void dfs(TreeNode root) {
+        if (root == null)
+            return;
         dfs(root.left);
-        l.add(root.val);
+        arr[p++] = root.val;
         dfs(root.right);
     }
 }
