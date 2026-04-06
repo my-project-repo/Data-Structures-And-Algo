7ecb6ab36f632bae073fcc6b56c1fde704ed3f56
my-project-repo <workwithimran007@gmail.com>
2026-04-06 15:28:45 +0530

    Time: 24 ms (20.10%) | Memory: 56.6 MB (51.87%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,18 +1,20 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int i = 0; i < dp.length; i++)
-            dp[i][0] = 1;
+        int dp [][] = new int[coins.length+1][amount+1];
+        for (int [] d : dp) Arrays.fill(d,-1);
+        return knap(0,coins,amount,dp);
+    }
+    int knap (int i , int [] coins , int amount , int [][] dp)
+    {
+        if (amount == 0) return 1;
+        else if (i >= coins.length) return 0;
+        else if (dp[i][amount] != -1) return dp[i][amount];
+        else if (coins[i] <= amount)
+        {
+            return dp[i][amount] = knap(i,coins,amount-coins[i],dp) + 
+            knap(i+1,coins,amount,dp);
 
-        for (int i = 1; i < dp.length; i++) {
-            int curr = coins[i - 1];
-            for (int j = 1; j < dp[0].length; j++) {
-                if (curr <= j) {
-                    dp[i][j] = dp[i][j - curr] + dp[i-1][j];
-                } else
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
-        return dp[coins.length][amount];
+        } else
+        return dp[i][amount] = knap(i+1,coins,amount,dp);
     }
 }
