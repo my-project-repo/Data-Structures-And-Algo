664157646d1f936e6f9c55168ae5d5917fa7141f
my-project-repo <workwithimran007@gmail.com>
2026-04-06 12:49:36 +0530

    Time: 31 ms (19.03%) | Memory: 48.1 MB (5.53%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,21 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int m = coins.length;
-        int [][] dp = new int[m+1][amount+1];
-        for (int [] d : dp) Arrays.fill(d,-1);
-        int res =  knap(0,coins,amount,dp);
-        return res == Integer.MAX_VALUE-1 ? -1 : res;
+        int[][] dp = new int[m + 1][amount + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int res = knap(m, coins, amount, dp);
+        return res == Integer.MAX_VALUE - 1 ? -1 : res;
     }
-    int knap (int i , int [] coins , int amount , int [][] dp)
-    {
-            if (amount == 0) return 0;
-    if (i >= coins.length) return Integer.MAX_VALUE-1;
-        else if (dp[i][amount] != -1) return dp[i][amount];
-        else if (coins[i] <= amount)
-        {
-            return dp[i][amount] = Math.min(1+knap(i,coins,amount-coins[i],dp), knap(i+1,coins,amount,dp));
+
+    int knap(int n, int[] coins, int amount, int[][] dp) {
+        if (amount == 0)
+            return 0;
+        else if (n <= 0)return Integer.MAX_VALUE - 1;
+        else if (dp[n][amount] != -1)
+            return dp[n][amount];
+        else if (coins[n-1] <= amount) {
+            return dp[n][amount] = Math.min(1 + knap(n, coins, amount - coins[n-1], dp), knap(n-1, coins, amount, dp));
         } else
-        return dp[i][amount] = knap(i+1,coins,amount,dp);
-        
+            return dp[n][amount] = knap(n - 1, coins, amount, dp);
+
     }
 }
