b59edbc70d7256f906f0c962a543881f883b5fdb
my-project-repo <workwithimran007@gmail.com>
2026-04-12 15:59:08 +0530

    Time: 12 ms (90.91%) | Memory: 44.4 MB (12.50%) - LeetSync

--- 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.java
+++ 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.java
@@ -1,24 +1,24 @@
 class Solution {
     public int subarrayLCM(int[] nums, int k) {
         int ans = 0;
-        for (int i = 0;i<nums.length;i++)
-        {
+        for (int i = 0; i < nums.length; i++) {
             int l = nums[i];
-            if (l == k) ans++;
-            for (int j = i+1;j<nums.length;j++)
-            {
-                l = lcm(l,nums[j]);
-                if (l == k) ans++;
-                if (l > k) break;
+            if (l == k)
+                ans++;
+            for (int j = i + 1; j < nums.length; j++) {
+                l = lcm(l, nums[j]);
+                if (l == k)
+                    ans++;
+                if (l > k)
+                    break;
             }
         }
         return ans;
     }
-    int lcm (int x , int y)
-    {
+
+    int lcm(int x, int y) {
         int pro = x * y;
-        while (y != 0)
-        {
+        while (y != 0) {
             int t = y;
             y = x % y;
             x = t;
