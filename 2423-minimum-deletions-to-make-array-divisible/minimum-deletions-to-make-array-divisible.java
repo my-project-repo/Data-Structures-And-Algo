60e44c1dc6051ece2767c8b81b2758677a87a838
my-project-repo <workwithimran007@gmail.com>
2026-04-12 16:10:52 +0530

    Time: 17 ms (72.29%) | Memory: 102.8 MB (9.54%) - LeetSync

---
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minOperations(int[] nums, int[] numsDivide) {
+        int gcd = numsDivide[0] , count = 0;
+        for (int i = 1;i<numsDivide.length;i++)
+        {
+            gcd = g(gcd,numsDivide[i]);
+        }
+        Arrays.sort(nums);
+        for (int i : nums)
+        {
+            if (gcd % i == 0) return count;
+            else count++;
+        }
+        return -1;
+    }
+    int g (int x , int y)
+    {
+        while (y != 0)
+        {
+            int t = y;
+            y = x % y;
+            x = t;
+        }
+        return x;
+    }
+}
