7444044088ed82c756e547331aca8983b6a7d277
my-project-repo <workwithimran007@gmail.com>
2026-04-14 19:59:06 +0530

    Time: 40 ms (60.22%) | Memory: 63.5 MB (69.78%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        char [] a = s.toCharArray();
+        char [] b = s.toCharArray();
+        int idx = 0 , edx = b.length-1;
+        int [][] dp = new int[a.length+1][b.length+1];
+        while (idx < edx)
+        {
+            char c = b[idx];
+            b[idx] = b[edx];
+            b[edx] = c;
+            idx++; edx--;
+        }
+        for (int [] d : dp) Arrays.fill(d,-1);
+        return lcs(a.length,b.length,a,b,dp);
+
+    }
+
+    int lcs(int i, int j, char[] a, char[] b, int[][] dp) {
+        if (i <= 0 || j <= 0)
+            return 0;
+        else if (dp[i][j] != -1)
+            return dp[i][j];
+        else if (a[i - 1] == b[j - 1])
+            return dp[i][j] = 1 + lcs(i - 1, j - 1, a, b, dp);
+        else {
+            return dp[i][j] = Math.max(lcs(i - 1, j, a, b, dp), lcs(i, j - 1, a, b, dp));
+        }
+    }
+}
