35454b6d03a53964d8aa272820bc34910195f8ae
my-project-repo <workwithimran007@gmail.com>
2026-04-28 00:30:25 +0530

    Time: 732 ms (38.83%) | Memory: 81.9 MB (10.72%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int minCut(String s) {
         int n = s.length();
+        char [] ch = s.toCharArray();
         int [][] dp = new int[n+1][n+1];
         for (int [] arr : dp) Arrays.fill(arr,-1);
-        return pp(0,n-1,s,dp);
+        return pp(0,n-1,ch,dp);
     }
-    int pp (int s , int e , String st , int [][]dp)
+    int pp (int s , int e , char [] st , int [][]dp)
     {
         if (s >= e) return 0;
         else if (isPal(s,e,st)) return 0;
@@ -21,11 +22,11 @@ class Solution {
         }
         return dp[s][e] = min;
     }
-    boolean isPal (int idx , int edx , String s)
+    boolean isPal (int idx , int edx , char [] s)
     {
         while (idx < edx)
         {
-            if (s.charAt(idx) != s.charAt(edx)) return false;
+            if (s[idx] != s[edx]) return false;
             idx++; edx--;
         }
         return true;
