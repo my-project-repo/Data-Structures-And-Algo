8aeaa83a88955e7c88f782b475180abbf6412261
my-project-repo <workwithimran007@gmail.com>
2026-05-08 12:46:17 +0530

    Time: 22 ms (39.02%) | Memory: 49.8 MB (16.90%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -3,21 +3,14 @@ class Solution {
         char[] ch = word.toCharArray();
         StringBuilder res = new StringBuilder();
         int i, count = 1;
-        for (i = 1; i < ch.length; i++) {
-            if (ch[i] == ch[i - 1]) {
+        for (i = 1; i <= ch.length; i++) {
+            while (i < ch.length && ch[i] == ch[i - 1] && count < 9) {
+                i++;
                 count++;
-                if (count == 9) {
-                    res.append("9").append(ch[i]);
-                    count = 0;
-                }
-            } else {
-                if (count > 0)
-                    res.append(String.valueOf(count)).append(ch[i - 1]);
-                count = 1;
             }
+            res.append(String.valueOf(count)).append(ch[i - 1]);
+            count = 1;
         }
-        if (count > 0)
-            res.append(String.valueOf(count)).append(ch[i - 1]); // last
         return res.toString();
     }
 }
