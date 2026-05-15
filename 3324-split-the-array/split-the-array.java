8e724c280ca1ee257e2e620673f1c5dea7ece645
my-project-repo <workwithimran007@gmail.com>
2026-05-16 01:38:15 +0530

    Time: 1 ms (99.25%) | Memory: 44.6 MB (60.28%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        int[] freq = new int[101];
+        int c = 0, d = 0;
+        for (int i : nums)
+            freq[i]++;
+        for (int i = 1; i < 101; i++) {
+            if (freq[i] == 0)
+                continue;
+            else if (freq[i] == 1)
+                c++;
+            else if (freq[i] % 2 != 0 || freq[i] != 2)
+                return false;
+            else
+                d++;
+        }
+        return c % 2 == 0;
+    }
+}
