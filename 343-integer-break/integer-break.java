9102a20617471377d7c19af3ee761a673be1a0f3
my-project-repo <workwithimran007@gmail.com>
2026-05-17 02:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.20%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,17 +1,22 @@
 class Solution {
     public int integerBreak(int n) {
-        int [][] dp = new int[n+1][n+1];
-        for (int [] d : dp) Arrays.fill(d,-1);
-        return helper(1,n,n,dp);
+        int[][] dp = new int[n + 1][n + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        return helper(1, n, n, dp);
     }
-    int helper (int idx , int n , int size , int [][] dp)
-    {
-        if (n == 0) return 1;
-        if (n < 0 || idx >= size) return 0;
-        if (dp[idx][n] != -1) return dp[idx][n];
-        if (idx > n ) return helper(idx+1,n,size,dp);
-        int include = idx * helper(idx,n-idx,size,dp);
-        int exclude = helper(idx+1,n,size,dp);
-        return dp[idx][n] = Math.max(include,exclude);
+
+    int helper(int idx, int n, int size, int[][] dp) {
+        if (n == 0)
+            return 1;
+        if (n < 0 || idx >= size)
+            return 0;
+        if (dp[idx][n] != -1)
+            return dp[idx][n];
+        if (idx > n)
+            return 0;
+        int include = idx * helper(idx, n - idx, size, dp);
+        int exclude = helper(idx + 1, n, size, dp);
+        return dp[idx][n] = Math.max(include, exclude);
     }
 }
