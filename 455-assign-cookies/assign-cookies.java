54799a3f451b25065be2b3262cb636ceb75ccde6
my-project-repo <workwithimran007@gmail.com>
2026-05-16 13:40:24 +0530

    Time: 5 ms (100.00%) | Memory: 51.1 MB (91.54%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,7 +1,18 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
+        Thread t1 = new Thread(() -> {Arrays.sort(g);});
+        Thread t2 = new Thread(() -> {Arrays.sort(s);});
+
+        t1.start();
+        t2.start();
+
+        try {
+        t1.join();
+        t2.join();
+        }
+        catch(InterruptedException e) {
+
+        }
         int idx = 0, idx2 = 0, edx = g.length, edx2 = s.length , res = 0;
         while (idx < edx && idx2 < edx2) {
             if (g[idx] > s[idx2])
