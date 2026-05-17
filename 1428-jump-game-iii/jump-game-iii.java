1968fb9590738a9305771eb8e6ab54c203cbc4cd
my-project-repo <workwithimran007@gmail.com>
2026-05-18 00:50:03 +0530

    Time: 3 ms (56.93%) | Memory: 53.1 MB (21.97%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        return search (arr,start,new boolean[arr.length]);
+    }
+
+    boolean search(int[] arr, int start, boolean[] isVisited) {
+        if (start < 0 || start >= arr.length)
+            return false;
+        else if (arr[start] == 0)
+            return true;
+        else if (isVisited[start]) return false;
+            isVisited[start] = true;
+            boolean x = search(arr, start - arr[start],isVisited);
+            boolean y = search(arr, start + arr[start],isVisited);
+            isVisited[start] = false;
+        return x || y ;
+    }
+}
