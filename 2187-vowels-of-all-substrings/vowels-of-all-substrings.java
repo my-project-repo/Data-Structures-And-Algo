bcd3e1c193e353289ae66d55238b81890d07c92e
my-project-repo <workwithimran007@gmail.com>
2026-05-19 01:38:57 +0530

    Time: 5 ms (100.00%) | Memory: 47 MB (24.06%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.java
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.java
@@ -1,17 +1,15 @@
 class Solution {
     public long countVowels(String word) {
+        boolean V[] = new boolean[26];
+        V['a' - 'a'] = V['e' - 'a'] = V['i' - 'a'] = V['o' - 'a'] = V['u' - 'a'] = true;
         long res = 0;
         int N = word.length();
         for (int i = 0; i < N; i++) {
-            if (isVowel(word.charAt(i))) {
-                res += (long)(N - i) * (i + 1);
+            if (V[word.charAt(i)- 'a']) {
+                res += (long) (N - i) * (i + 1);
             }
         }
 
         return res;
-    }
-
-    boolean isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }
