cc67cc37baef9f75fb76ba81f279afaa4748708d
my-project-repo <workwithimran007@gmail.com>
2026-05-19 16:05:59 +0530

    Time: 768 ms (37.93%) | Memory: 42.9 MB (77.92%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -2,27 +2,23 @@ class Solution {
     public int minCut(String s) {
         int n = s.length();
         char[] ch = s.toCharArray();
-        int[][] dp = new int[n + 1][n + 1];
-        for (int[] arr : dp)
-            Arrays.fill(arr, -1);
-        return pp(0, n - 1, ch, dp);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return pp(0, ch, dp) - 1;
     }
 
-    int pp(int s, int e, char[] st, int[][] dp) {
-        if (s >= e)
+    int pp(int i, char[] ch, int[] dp) {
+        if (i >= ch.length)
             return 0;
-        else if (isPal(s, e, st))
-            return 0;
-        else if (dp[s][e] != -1)
-            return dp[s][e];
-        int min = Integer.MAX_VALUE;
-        for (int k = s; k < e; k++) {
-            if (isPal(s, k, st)) {
-                int count = 1 + pp(k + 1, e, st, dp);
-                min = Math.min(min, count);
+        if (dp[i] != -1)
+            return dp[i];
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k < ch.length; k++) {
+            if (isPal(i, k, ch)) {
+                ans = Math.min(ans, 1 + pp(k + 1, ch, dp));
             }
         }
-        return dp[s][e] = min;
+        return dp[i] = ans;
     }
 
     boolean isPal(int idx, int edx, char[] s) {
