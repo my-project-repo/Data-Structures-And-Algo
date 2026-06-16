00e4365b5581ab8fb0f85cf7369b628aa3d3cda1
Shaikh Imran <workwithimran007@gmail.com>
2026-06-16 22:21:56 +0530

    Time: 3 ms (100.00%) | Memory: 55.2 MB (64.71%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -3,15 +3,14 @@ class Solution {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (!str.isEmpty()) {
-                if (ch == '#')
-                    str.append(str);
-                else if (ch == '*')
-                    str.deleteCharAt(str.length() - 1);
-                else if (ch == '%')
-                    str.reverse();
-            }
-            if (Character.isLetter(ch))
+            if (ch == '#')
+                str.append(str);
+            else if (ch == '*') {
+                if (str.length() > 0)
+                str.deleteCharAt(str.length() - 1);
+            } else if (ch == '%')
+                str.reverse();
+            else
                 str.append(ch);
         }
 
