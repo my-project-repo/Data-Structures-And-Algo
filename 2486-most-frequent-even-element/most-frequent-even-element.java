adf62d8bf244bff5f6427744ddab0e62b459258b
Shaikh Imran <workwithimran007@gmail.com>
2026-06-20 19:27:00 +0530

    Time: 18 ms (55.64%) | Memory: 47.8 MB (10.29%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -12,7 +12,6 @@ class Solution {
         int maxFreq = 0;
 
         for (int i = 0; i < freq.length; i++) {
-            if (freq[i] == 0) continue;
             if (freq[i] > maxFreq) {
                 maxFreq = freq[i];
                 ans = i;
