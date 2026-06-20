2a46b7e6692412715e4572c04769d80ef00c20e1
Shaikh Imran <workwithimran007@gmail.com>
2026-06-20 19:25:11 +0530

    Time: 19 ms (43.95%) | Memory: 47.6 MB (43.82%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        int[] freq = new int[100001];
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                freq[num]++;
+            }
+        }
+
+        int ans = -1;
+        int maxFreq = 0;
+
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > maxFreq) {
+                maxFreq = freq[i];
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+}
