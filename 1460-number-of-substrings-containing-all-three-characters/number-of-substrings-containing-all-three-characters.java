91be660fca974a06e41305bb82ad028c2cd23c13
Shaikh Imran <workwithimran007@gmail.com>
2026-07-03 22:39:28 +0530

    Time: 10 ms (95.21%) | Memory: 46.6 MB (14.55%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,21 +1,27 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int numberOfSubstrings(String s) {
-        // bruteForce
-        int[] hash = { -1, -1, -1 };
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            hash[s.charAt(i) - 'a'] = i;
-            if (hash[0] != -1 && hash[1] != -1 && hash[2] != -1)
-                res += 1 + (min(hash[0], hash[1], hash[2]));
-
+        int a = 0, b = 0, c = 0, res = 0, l = 0 , n = s.length();
+        for (int r = 0; r < n; r++) {
+            char ch = s.charAt(r);
+            if (ch == 'a')
+                a++;
+            else if (ch == 'b')
+                b++;
+            else
+                c++;
+            while (a > 0 && b > 0 && c > 0) {
+                res += n - r;
+                char left = s.charAt(l);
+                if (left == 'a')
+                    a--;
+                else if (left == 'b')
+                    b--;
+                else
+                    c--;
+                l++;
+            }
         }
-        return res;
-    }
 
-    int min(int a, int b, int c) {
-        return Math.min(Math.min(a, b), c);
+        return res;
     }
 }
