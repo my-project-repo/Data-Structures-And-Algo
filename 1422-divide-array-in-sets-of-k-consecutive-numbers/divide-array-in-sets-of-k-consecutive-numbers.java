d944d45cab460e8bb83f57bddb6cee9a584c80ec
Shaikh Imran <workwithimran007@gmail.com>
2026-07-13 03:15:36 +0530

    Time: 398 ms (18.99%) | Memory: 92.8 MB (34.00%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -14,10 +14,11 @@ class Solution {
 
     boolean next(int i, int[] nums, int k) {
         int d = nums[i] + 1, c = 1;
-        nums[i] = -1;
         i++;
-        while (i < nums.length && c < k) {
-            if (nums[i] == d) {
+        while (i < nums.length && c < k)
+        {
+            if (nums[i] == d)
+            {
                 d++;
                 nums[i] = -1;
                 c++;
