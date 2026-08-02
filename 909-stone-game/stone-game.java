4a98d5a511c267cdf89b755d8461fe2f663db04b
Shaikh Imran <workwithimran007@gmail.com>
2026-08-03 01:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (49.04%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,28 @@
 class Solution {
-    public boolean stoneGame(int[] nums) {
-        return true;
+    public boolean stoneGame(int[] piles) {
+        int idx = 0, edx = piles.length - 1, alice = 0, bob = 0;
+        boolean myTurn = true;
+        while (idx < edx) {
+
+            if (myTurn) {
+                if (piles[idx] > piles[edx]) {
+                    alice += piles[idx++];
+                } else {
+                    alice += piles[edx--];
+                }
+            } else {
+
+                if (piles[idx] > piles[edx]) {
+                    bob += piles[edx--];
+                } else {
+                    bob += piles[idx++];
+                }
+
+            }
+
+            myTurn = !myTurn;
+        }
+
+        return alice > bob;
     }
 }
