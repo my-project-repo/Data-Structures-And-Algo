179bad1ea26ffa5e1f564600cd461cb06a955f65
Sk Imran <workwithimran007@gmail.com>
2026-08-17 23:47:44 +0530

    Time: 42 ms (84.35%) | Memory: 48.5 MB (29.93%) - LeetSync

--- 4034-maximum-gap-between-stations/maximum-gap-between-stations.java
+++ 4034-maximum-gap-between-stations/maximum-gap-between-stations.java
@@ -1,21 +1,22 @@
 class Solution {
     public int maximumGap(String skill, String station) {
         int m = skill.length(), n = station.length(), pos = 0, ans = 0;
-        int[][] freq = new int[m][2]; // start - end
+        int [] left = new int[m];
+        int [] right = new int[m];
         for (int i = 0; i < n && pos < m; i++) {
             if (skill.charAt(pos) == station.charAt(i)) {
-                freq[pos++][0] = i;
+                left[pos++] = i;
             }
         }
         pos = m - 1;
         for (int i = n - 1; i >= 0 && pos >= 0; i--) {
             if (skill.charAt(pos) == station.charAt(i)) {
-                freq[pos--][1] = i;
+                right[pos--] = i;
             }
         }
 
         for (int i = 1; i < m; i++) {
-            ans = Math.max(ans, freq[i][1] - freq[i - 1][0]);
+            ans = Math.max(ans, right[i] - left[i-1]);
         }
 
         return ans;
