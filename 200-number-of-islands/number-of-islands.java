4121febb5a5ef693724c95bb2f7ed169da8eb9d1
Sk Imran <workwithimran007@gmail.com>
2026-09-11 10:13:56 +0530

    Time: 6 ms (21.22%) | Memory: 51 MB (98.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,30 +1,35 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-
-        int m = grid.length, n = grid[0].length;
-        int ans = 0;
+        int m = grid.length, n = grid[0].length , ans = 0;
+        int[][] dir = {
+                { 0, 1 },
+                { 0, -1 },
+                { 1, 0 },
+                { -1, 0 }
+        };
+        Queue<int[]> q = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j, m, n);
                     ans++;
+                    q.add(new int[] { i, j });
+                    grid[i][j] = '0';
+                    while (!q.isEmpty()) {
+                        int[] val = q.poll();
+                        for (int[] d : dir) {
+                            int nr = val[0] + d[0];
+                            int nc = val[1] + d[1];
+                            if (nr < 0 || nr >= m || nc < 0 || nc >= n || grid[nr][nc] == '0')
+                                continue;
+
+                            grid[nr][nc] = '0';
+                            q.add(new int[] { nr, nc });
+                        }
+                    }
                 }
             }
         }
 
         return ans;
-    }
-
-    private void dfs(char[][] grid, int i, int j, int len, int wid) {
-        if (i >= 0 && i < len && j >= 0 && j < wid && grid[i][j] == '1') {
-            grid[i][j] = '0';
-            dfs(grid, i - 1, j, len, wid);
-            dfs(grid, i, j + 1, len, wid);
-            dfs(grid, i + 1, j, len, wid);
-            dfs(grid, i, j - 1, len, wid);
-        }
     }
 }
