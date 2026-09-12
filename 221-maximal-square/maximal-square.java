6d900098f287a49e69e8f6158db2e7389d289270
Sk Imran <workwithimran007@gmail.com>
2026-09-13 00:20:10 +0530

    Time: 8 ms (75.61%) | Memory: 70.9 MB (74.32%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,28 +1,21 @@
 class Solution {
-    Integer[][] dp;
-
     public int maximalSquare(char[][] matrix) {
         int ans = 0, m = matrix.length, n = matrix[0].length;
-        dp = new Integer[m][n];
+        int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
-                    ans = Math.max(ans, dfs(i, j, matrix));
+                    if (i == 0 || j == 0)
+                        dp[i][j] = 1;
+                    else {
+                        dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+                    }
+
+                    ans = Math.max(ans,dp[i][j]);
                 }
             }
         }
 
         return ans * ans;
-    }
-
-    int dfs(int r, int c, char[][] M) {
-        if (r >= M.length || c >= M[0].length || M[r][c] == '0')
-            return 0;
-        if (dp[r][c] != null)
-            return dp[r][c];
-        int down = 1 + dfs(r + 1, c, M);
-        int right = 1 + dfs(r, c + 1, M);
-        int diagonal = 1 + dfs(r + 1, c + 1, M);
-        return dp[r][c] = Math.min(Math.min(down, right), diagonal);
     }
 }
