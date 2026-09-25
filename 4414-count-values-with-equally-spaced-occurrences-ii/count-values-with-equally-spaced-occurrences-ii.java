75e8f727ff7ff8fe94502430ba35925034bb7941
Sk Imran <workwithimran007@gmail.com>
2026-09-26 00:41:52 +0530

    Time: 92 ms (61.20%) | Memory: 247.3 MB (60.44%) - LeetSync

--- 4414-count-values-with-equally-spaced-occurrences-ii/count-values-with-equally-spaced-occurrences-ii.java
+++ 4414-count-values-with-equally-spaced-occurrences-ii/count-values-with-equally-spaced-occurrences-ii.java
@@ -1,47 +1,35 @@
 class Solution {
     public int countSpecialIntegers(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        int ans = 0;
-        for (int i : nums) { // O(N)
-            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        HashMap<Integer , List<Integer>> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            map.computeIfAbsent(nums[i] , k -> new ArrayList<>()).add(i);
         }
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) { // O(N)
-            int v = nums[i];
-            if (freq.get(v) >= 3) {
-                if (map.containsKey(v)) {
-                    List<Integer> list = map.get(v);
-                    list.add(i);
-                    map.put(v, list);
-                } else {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(i);
-                    map.put(v, list);
-                }
+        int answer = 0;
+
+        for(List<Integer> indexes : map.values()){
+            if(indexes.size() < 3){
+                continue;
             }
-        }
 
-        for (int key : map.keySet()) {
-
-            List<Integer> list = map.get(key);
-
-            int gap = list.get(1) - list.get(0);
+            int gap = indexes.get(1) - indexes.get(0);//gap calculation
 
             boolean special = true;
 
-            for (int i = 2; i < list.size(); i++) {
-                if (list.get(i) - list.get(i - 1) != gap) {
+            //check for the remaining gaps
+            for(int i = 2 ; i < indexes.size() ; i++){
+                int currentGap = indexes.get(i) - indexes.get(i - 1);
+
+                if(currentGap != gap){
                     special = false;
                     break;
                 }
             }
-
-            if (special) {
-                ans++;
+            if(special){
+                answer++;
             }
         }
-
-        return ans;
+        return answer;
     }
 }
